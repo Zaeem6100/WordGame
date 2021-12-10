8e22b5903e38f44d8b3d27ad05cd3bdc0ac7d99e
Zaeem <32678310+Zaeem6100@users.noreply.github.com>
2021-12-10 17:10:38 +0500

    Initial commit

---
+++ src/Loading_Data.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.LinkedHashSet;
+
+public class Loading_Data {
+    private LinkedHashSet<String> hs = new LinkedHashSet<String>();
+
+    public Loading_Data() {
+        this.hs = null;
+    }
+
+    public LinkedHashSet<String> ReadData(String filename) throws IOException {
+        File file = new File(filename);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        StringBuffer sb = new StringBuffer();
+        String line;
+        while ((line = br.readLine()) != null) {
+            hs.add(line);
+        }
+        return hs;
+    }
+
+    public LinkedHashSet<String> getHs() {
+        return hs;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Loading_Data loader = new Loading_Data();
+        loader.ReadData("list");
+
+    }
+}
