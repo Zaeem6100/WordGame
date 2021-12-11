4bb35feda183ffec501462beaa7dc7e61f45141f
Zaeem <32678310+Zaeem6100@users.noreply.github.com>
2021-12-12 01:18:04 +0500

    code

--- src/wordGame.java
+++ src/wordGame.java
@@ -8,25 +8,33 @@ public class wordGame {
     private  boolean outcome = false;
     private  boolean continueORterminate = true;
     private   Scanner in = new Scanner(System.in);
-    private  void gameStart(){
+    public   void gameStart(){
         String st = "empathy";
         System.out.println("Word Game started with empathy ");
         do {
             String lastletter = st.substring(st.length() - 1);
             String inp = Input();
-            if (inp.charAt(0) != lastletter.charAt(0)){
+            if (inp.charAt(0) != lastletter.charAt(0) ){
                 System.out.println("“Entered word is invalid or does not exist in the \n" +
                         "dictionary, please try again”");
+                if (st == words.get(words.size() - 1)){
+                    gameRestart();
+                    return;
+                }
             }
+
+
         }while (continueORterminate);
     }
-    private  void gameRestart(){
+    public   void gameRestart(){
         words.clear();
         outcome = false;
         continueORterminate = true;
+        System.out.println("Game Restarted ");
+        gameStart();
     }
 
-    private  boolean wordValid(String st) throws IOException {
+    public   boolean wordValid(String st) throws IOException {
         Loading_Data loader = new Loading_Data();
         LinkedHashSet<String> hs =loader.ReadData("dictionary.txt");
         return  hs.contains(st) ? true :false;
@@ -34,10 +42,24 @@ public class wordGame {
 
     private  String Input(){
 
-        System.out.println("Enter a word (caseSensitive) to play the game, such as \n" +
-                "empathy->yet->top...: Yes");
-        String line = in.next();
-        return line;
+        String line;
+        do {
+            System.out.println("Enter a word (caseSensitive) to play the game, such as \n" +
+                    "empathy->yet->top...: Yes");
+            line = in.next();
+            if(isStringOnlyAlphabet(line)){
+                return line;
+            }else {
+                System.out.println("Wrong input !!!! input must be Characters only");
+            }
+        }while (true);
+    }
+
+    private  boolean isStringOnlyAlphabet(String str)
+    {
+        return ((str != null)
+                && (!str.equals(""))
+                && (str.matches("^[a-zA-Z]*$")));
     }
 
 
